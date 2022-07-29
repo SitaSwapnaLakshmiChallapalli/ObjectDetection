42d6de495b3cb97547826dddb6fae932011f71cf
SIta Challapalli <85593091+SitaSwapnaLakshmiChallapalli@users.noreply.github.com>
2022-07-29 11:13:28 -0500

    Create simple_facerec.py

---
+++ simple_facerec.py
@@ -0,0 +1,52 @@
+import face_recognition
+import cv2
+import os
+import glob
+import numpy as np
+
+class SimpleFacerec:
+    def __init__(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+        self.frame_resizing = 0.25
+
+    def load_encoding_images(self, images_path):
+        """
+        Loading encoding images from path
+        :param images_path:
+        :return:
+        """
+        # Loading Images
+        images_path = glob.glob(os.path.join(images_path, "*.*"))
+
+        print("{} encoding images found.".format(len(images_path)))
+
+
+        for img_path in images_path:
+            img = cv2.imread(img_path)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            basename = os.path.basename(img_path)
+            (filename, ext) = os.path.splitext(basename)
+            img_encoding = face_recognition.face_encodings(rgb_img)[0]
+            self.known_face_encodings.append(img_encoding)
+            self.known_face_names.append(filename)
+        print("Encoding images loaded")
+
+    def detect_known_faces(self, frame):
+        small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            name = "Unknown"
+            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = self.known_face_names[best_match_index]
+            face_names.append(name)
+        face_locations = np.array(face_locations)
+        face_locations = face_locations / self.frame_resizing
+        return face_locations.astype(int), face_names
